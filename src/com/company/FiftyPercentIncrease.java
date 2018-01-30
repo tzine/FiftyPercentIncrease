b1a6bc579443570d924e1a0bbca24a14c95bd5c5
tzine <34639191+tzine@users.noreply.github.com>
2018-01-29 22:52:09 -0500

    Initial commit

---
+++ src/com/company/FiftyPercentIncrease.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class FiftyPercentIncrease {
+
+    public static void main(String[] args) {
+	int number = 2;
+	double finnum;
+	finnum = (2 * .50) + 2;
+	System.out.println("Fifty percent more than, " + number + " is " + finnum + "!");
+    }
+}
